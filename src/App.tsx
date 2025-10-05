d87b19a4332b4a7896365f2b47af6036fe132225
Higor <higoraraujo_pb@outlook.com>
2025-10-05 20:16:28 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.module.css'
 import BattleScreen from './components/BattleScreen/BattleScreen'
 import BattleLog from './components/BattleLog/BattleLog'
 import style from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <main className={style.mainContainer}>
-        <div className={style.gameInterface}>
-          <BattleScreen></BattleScreen>
-          <BattleLog></BattleLog>
+        <div className={style.leftSide}>
+            <BattleScreen></BattleScreen>
         </div>
+          <div className={style.rightSide}>
+            <BattleLog></BattleLog>
+          </div>
       </main>
     </>
   )
